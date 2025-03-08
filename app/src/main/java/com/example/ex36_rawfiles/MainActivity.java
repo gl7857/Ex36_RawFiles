b638665971cbd081fa0479fefb3aadb57a5e6b0a
gl7857 <gl7857@bs.amalnet.k12.il>
2025-03-08 21:37:28 +0200

    Ex36_RawFiles

--- app/src/main/java/com/example/ex36_rawfiles/MainActivity.java
+++ app/src/main/java/com/example/ex36_rawfiles/MainActivity.java
@@ -6,15 +6,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
